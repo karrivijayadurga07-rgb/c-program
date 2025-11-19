f6905df6a4a870be8c54f6d13eef71f8ac3ff22f
karrivijayadurga07-rgb <karrivijayadurga07@gmail.com>
2025-11-19 21:05:34 +0530

    Add files via upload

---
+++ lower triangular - 7e(1).c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int r,i,j;
+	printf("Enter the number of rows columns of square matrix:");
+	scanf("%d",&r);
+	int a[r][r];
+	printf("Enter the elements of matrix:\n");
+	for(i=0;i<r;i++)
+	for(j=0;j<r;j++)
+	scanf("%d",&a[i][j]);
+	// checking
+	int is_lower_triangular=1;
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			if(j>i && a[i][j]!=0 || j<=i && a[i][j]==0)
+			{
+				is_lower_triangular=0;
+				break;
+			}
+		}
+	}
+	if(is_lower_triangular){
+	printf("Given matrix is a lower triangular matrix");
+	}else{
+		printf("Given matrix is not an lower triangular matrix");
+	}
+}
